253d4e719ada87c4cf91988bc738066d34a085a6
Hugo Simon <hugo.simon@etu.inp-n7.fr>
2023-12-04 10:54:53 +0100

    day04 solved clean

--- day04/part2.py
+++ day04/part2.py
@@ -5,17 +5,13 @@ cards = [k.split(":")[1:] for k in content]
 cards = [k[0].split("|") for k in cards]
 cards = [[k[0].split(), k[1].split()] for k in cards]
 
-size = len(cards[0][1])
-nb_card = 0
+nb_card = [1]*len(cards)
 for i in range(len(cards)):
     matching = 0
-    nb_card += 1
-    for card in cards[i][1][:size]:
+    for card in cards[i][1]:
         if card in cards[i][0]:
             matching += 1
-    ratio = len(cards[i][1]) // size
     for j in range(1, matching+1):
-        nb_card += ratio
-        cards[i+j][1] += cards[i+j][1][:size]*ratio
+        nb_card[i+j-1] += nb_card[i-1]
 
-print(nb_card)
+print(sum(nb_card))
